535e6a754535d186ab8062d562c4a4ddb5d33c7c
jonjigoncalves <jonjigoncalves@gmail.com>
2023-06-13 11:44:45 -0400

    working through form

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,6 +1,9 @@
 // // first goal is to connect start button
 var startBtn = document.querySelector('#start-btn')
 var quizwrap = document.querySelector('.quiz-wrap');
+var scoresDiv = document.querySelector('footer.scores');
+
+
 
 var score = 0;
 var timeleft = 60;
@@ -10,12 +13,13 @@ startBtn.addEventListener('click', function () {
   quizwrap.classList.remove('hide');
   startGame();
   setInterval(clockTick, 1000);
- 
+
 })
 function clockTick() {
   timeleft--;
   if (timeleft < 0) {
     timeleft = 0;
+    endGame();
   }
   document.getElementById('timer-el').innerText = timeleft
 
@@ -27,63 +31,153 @@ function startGame() {
   // show current question using the index
   showQuestion();
 
-  console.log('start!');
+  // console.log('start!');
 }
 function showQuestion() {
-  console.log(questionData);
+  // console.log(questionData);
   // get the question object of the current one from the index(currentQuestionIndex) and store it to a variable
+  if (currentQuestionIndex >= questionData.length) {
+    
+    return endGame();
+  }
+  
+  
   var questionObj = questionData[currentQuestionIndex];
   console.log(questionObj);
+  console.log(currentQuestionIndex);
   var questionOutputEl = document.getElementById('question-output');
   questionOutputEl.innerText = questionObj.question;
   var choicesOutputEl = document.querySelector('.choices');
   choicesOutputEl.innerHTML = "";
-  for (index = 0; index < questionObj.options.length; index++) {
+  // endGame();
+  
+  for (var index = 0; index < questionObj.options.length; index++) {
     var button = document.createElement('button');
     button.innerText = questionObj.options[index];
     button.setAttribute('value', questionObj.options[index])
     button.onclick = function () {
       if (this.value === questionObj.answer) {
-        score++
+        score += 100
+        console.log('Correct');
         console.log(score);
       }
       else {
         timeleft -= 10;
-        console.log(timeleft);
+        console
+        // console.log(timeleft);
       }
+      
       currentQuestionIndex++;
-      if (currentQuestionIndex < questionData.length) {
-        showQuestion();
-      }
-      else {
-        endGame();
-      }
-
+      showQuestion();
     }
-
-
-    choicesOutputEl.append(button);
-
+    
+      choicesOutputEl.append(button);
+      
 
   }
+
 }
 function endGame() {
   document.getElementById('quiz-main').setAttribute('class', 'hide');
   document.querySelector('.end').classList.remove('hide');
-
  
+
 }
-  
+
 
 
 document.getElementById('initial-form').addEventListener('submit', function (event) {
   event.preventDefault();
-  console.log(event.target);
+  // console.log(event.target);
   var initials = document.getElementById('initials').value
-  console.log(initials);
+  // console.log(initials);
+
+  results();
+
 })
-// startBtn.addEventListener('click', startGame);
 
 
+
+function getUserScores() {
+  showQuestion();
+  var rawData = localStorage.getItem('initials');
+  var parsed = JSON.parse(rawData) || [];
+
+  if (!Array.isArray(parsed)) {
+    return [];
+  }
+
+  return parsed;
+}
+
+function saveUserData(arr) {
+  var jsonVal = JSON.stringify(arr);
+
+  localStorage.setItem('initials', jsonVal);
+}
+
+
+document.getElementsByClassName('over').addEventListener('click', function(){
+  document.querySelector('.end').classList.add('hide');
+})
+
+// issues with the div in end coming back after a few seconds, added a prevent default but it seems to just have delayed it, i suspect the endgame function is the issue as that is where the class hide is removed so the user can add there name before the score is displayed
+
+function results() {
+  // Hide the form
+  var form = document.getElementById('initial-form');
+  form.classList.add('hide');
+
+  
+
+  // Display the scores
+  var scoresDiv = document.querySelector('footer.scores');
+  scoresDiv.classList.remove('hide');
+
+  // Create and append score elements
+  var initials = document.getElementById('initials').value;
+  var userScore = {
+    name: initials,
+    score: score
+  };
+
+  var usersArray = getUserScores();
+  usersArray.push(userScore);
+  saveUserData(usersArray);
+
+  showScores();
+
+  var endDiv = document.querySelector('.end');
+endDiv.classList.add('hide');
+// prevent 
+preventDefault();
+}
+
+
+function showScores() {
+  var users = getUserScores();
+  var scoresDiv = document.querySelector('footer.scores');
+
+  for (var i=0; i<users.length; i++){
+  // for (var userObj of users) {
+    var userObj = users[i];
+    var div = document.createElement('div');
+
+    div.classList.add('score');
+    scoresDiv.appendChild(div);
+
+    var h2 = document.createElement('h2');
+    h2.innerText = 'Name: ' + userObj.name;
+    div.append(h2);
+
+    var p = document.createElement('p');
+    p.innerText = 'Score: ' + userObj.score;
+    div.append(p);
+
+
+  }
+  
+}
+showScores();
 
 
